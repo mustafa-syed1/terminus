6c3c14ed21aac332068384bb09ae2c1b90023383
mustafa-syed <muhammad.hasan@gmail.com>
2017-08-31 15:20:30 +0500

    Added comments

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -27,6 +27,9 @@ namespace WpfApplication1
             SomeSetupHere();
         }
 
+        /// <summary>
+        /// register context menu with label
+        /// </summary>
         private void SomeSetupHere()
         {
             ContextMenu cm = new ContextMenu();
@@ -43,31 +46,61 @@ namespace WpfApplication1
             label3.ContextMenu = cm;
         }
 
+        /// <summary>
+        /// Mouse left button down event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lable0MouseLtBtnDown(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("Aha, left button is fun!");
         }
 
+        /// <summary>
+        /// Mouse enter event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lable1MouseEnter(object sender, MouseEventArgs e)
         {
             MessageBox.Show("Mouse, this is restricted text!");
         }
 
+        /// <summary>
+        /// Mouse double click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lable2MouseDblClick(object sender, MouseButtonEventArgs e)
         {
             MessageBox.Show("Ouch, double click hurts!");
         }
 
+        /// <summary>
+        /// Mouse right button up event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lable3MouseRtBtnUp(object sender, MouseButtonEventArgs e)
         {
             label3.ContextMenu.IsOpen = true;
         }
 
+        /// <summary>
+        /// Mouse Leave event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lable4MouseLeave(object sender, MouseEventArgs e)
         {
             MessageBox.Show("Bye!");
         }
 
+        /// <summary>
+        /// Mouse move event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void lable5MouseMove(object sender, MouseEventArgs e)
         {
             MessageBox.Show("Hehe, its funny");
